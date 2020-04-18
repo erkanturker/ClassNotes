4b6af78a68d78237c0a8c0b193ea34da9a8af392
erkanturker <erkanturker17@gmail.com>
2020-04-18 13:44:37 -0400

    first commit

---
+++ src/main/java/tests/MyFirstAutomationScript.java
@@ -0,0 +1,14 @@
+package tests;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstAutomationScript {
+    public static void main (String[] args) {
+        WebDriverManager.chromedriver ().setup ();
+        ChromeDriver driver= new ChromeDriver (  );
+        driver.get ( "http://google.com" );
+
+    }
+}
